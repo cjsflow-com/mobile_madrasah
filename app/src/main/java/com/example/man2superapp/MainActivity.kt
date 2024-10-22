6f7096926be337b5b618dbb29a770afbf7d2d770
Said Shodiq Mufadhol <shodiqmufadhol1.com>
2024-10-22 07:32:15 +0700

    mencoba menambahkan beberapa fungsionalitas

--- app/src/main/java/com/example/man2superapp/MainActivity.kt
+++ app/src/main/java/com/example/man2superapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.man2superapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -15,6 +17,30 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Inisialisasi CardView
+        val ekinCard: CardView = findViewById(R.id.ekinCard)
+        val jurnalCard: CardView = findViewById(R.id.jurnalCard)
+        val songketCard: CardView = findViewById(R.id.songketCard)
+        val etatibCard: CardView = findViewById(R.id.etatibCard)
+        val wbsCard: CardView = findViewById(R.id.wbsCard)
+        val pelajarCard: CardView = findViewById(R.id.pelajarCard)
+
+        // Set OnClickListener pada card
+        ekinCard.setOnClickListener {
+            val intent = Intent(this, EkinActivity::class.java)
+            startActivity(intent)
+        }
+
+        songketCard.setOnClickListener {
+            val intent = Intent(this, SongketActivity::class.java)
+            startActivity(intent)
+        }
+
+        wbsCard.setOnClickListener {
+            val intent = Intent(this, wbs::class.java)
+            startActivity(intent)
         }
     }
 }
